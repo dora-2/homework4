f9a9c4ea56a7e33b54938972ea79dda30229f9e7
dora <zyazevad@mail.ru>
2024-10-10 16:14:19 +0300

    fun

--- src/processing.py
+++ src/processing.py
@@ -1,3 +1,7 @@
-def filter_by_state(dictionary: list) -> list:
+def filter_by_state(dictionary: list, state = ['EXECUTED'], d = []) -> list:
+    for i in dictionary:
+        if i['state'] == state:
+            d += i
+    return i
 
-    return
+print(filter_by_state([{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'}, {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}]))
